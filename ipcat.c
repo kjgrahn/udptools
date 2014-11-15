kjgrahn/udptools

commit efa1f670a8944a5b6ce2f1a48ab1716f9dfd9dd1

ipcat: IPv6 support
Turns out it's trivial to support IPv6 for raw IP traffic -- if
you just don't try to force IPv4.  Sigh.

ipcat.c
@@ -3,7 +3,7 @@
  *         instead of UDP, and with hex input so it's more
  *         feasible to send any data
  *
- * Copyright (c) 2013 Jörgen Grahn
+ * Copyright (c) 2013, 2014 Jörgen Grahn
  * All rights reserved.
  *
  */
@@ -46,7 +46,7 @@ static void cli_create(struct Client* const this,
     }
 
     const struct addrinfo hints = { AI_ADDRCONFIG | AI_CANONNAME,
-				    AF_INET,
+				    AF_INET6,
 				    SOCK_RAW,
 				    pent->p_proto,
 				    0, 0, 0, 0 };

udpcat.1
@@ -4,7 +4,7 @@
 ..
 .
 .
-.TH udpcat 1 "FEB 2014" UDPTOOLS "User Manuals"
+.TH udpcat 1 "NOV 2014" UDPTOOLS "User Manuals"
 .SH "NAME"
 udpcat \- general UDP datagram sender
 .
@@ -70,8 +70,7 @@ not just numerical addresses and port numbers.
 is similar, but lets you send raw IPv4 datagrams carrying
 .I protocol
 instead. I.e. the hex dump lines provided as input becomes
-IPv4 payload rather than UDP payload.
-IPv6 is not supported here.
+IP (IPv6 or IPv4) payload rather than UDP payload.
 .PP
 The
 .I protocol

commit af1e3533d4cc99edd9587e3795ece69d828e99e9

ipcat: bug fix
efa1f67 broke the IPv4 support, by forcing IPv6.  Of course I
meant to let the destination address control the protocol choice.

ipcat.c
@@ -62,7 +62,7 @@ static void cli_create(struct Client* const this,
     }
 
     const struct addrinfo hints = { AI_ADDRCONFIG | AI_CANONNAME,
-				    AF_INET6,
+				    AF_UNSPEC,
 				    SOCK_RAW,
 				    pn,
 				    0, 0, 0, 0 };